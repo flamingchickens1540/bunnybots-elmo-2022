f3173be4a521abbbef4bcaa66fdf5b1c0e0a1f94
MeechaPooch <powchm@catlin.edu>
2022-11-19 15:11:09 -0800

    remove unused import

--- src/main/java/org/team1540/elmo/utils/WaitDashboardVarCommand.java
+++ src/main/java/org/team1540/elmo/utils/WaitDashboardVarCommand.java
@@ -2,7 +2,6 @@ package org.team1540.elmo.utils;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import org.team1540.elmo.subsystems.ejectors.Ejectors;
 
 public class WaitDashboardVarCommand extends WaitVariableCommand {
     public WaitDashboardVarCommand(Command command, String processName, Double defaultValue) {
