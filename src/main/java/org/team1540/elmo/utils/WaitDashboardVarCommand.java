57fcb5024fe4bd7e75121cdba3ca604dcd030136
MeechaPooch <powchm@catlin.edu>
2022-11-19 15:30:56 -0800

    dont reset smartdashboard key each time a WaitDashboardVarCommand is constructed

--- src/main/java/org/team1540/elmo/utils/WaitDashboardVarCommand.java
+++ src/main/java/org/team1540/elmo/utils/WaitDashboardVarCommand.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class WaitDashboardVarCommand extends WaitVariableCommand {
     public WaitDashboardVarCommand(Double defaultValue, String processName, Command command) {
         super(()->SmartDashboard.getNumber(command.getName() + "/" + processName + "-wait",defaultValue));
-        SmartDashboard.putNumber(command.getName() + "/" + processName + "-wait",defaultValue);
+        String smartDashboardKey = command.getName() + "/" + processName + "-wait";
+        SmartDashboard.putNumber(smartDashboardKey,SmartDashboard.getNumber(smartDashboardKey,defaultValue)); // put key if it doesn't already exist
     }
 }
