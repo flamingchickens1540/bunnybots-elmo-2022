3187661974eb1c4725ef183e24158651a5abbcd2
MeechaPooch <powchm@catlin.edu>
2022-11-19 15:37:40 -0800

    WaitDashboardVarCommand with null command arg uses global key

--- src/main/java/org/team1540/elmo/utils/WaitDashboardVarCommand.java
+++ src/main/java/org/team1540/elmo/utils/WaitDashboardVarCommand.java
@@ -5,8 +5,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class WaitDashboardVarCommand extends WaitVariableCommand {
     public WaitDashboardVarCommand(Double defaultValue, String processName, Command command) {
-        super(()->SmartDashboard.getNumber(command.getName() + "/" + processName + "-wait",defaultValue));
-        String smartDashboardKey = command.getName() + "/" + processName + "-wait";
+        super(()->SmartDashboard.getNumber(getDashboardKey(command,processName),defaultValue));
+        String smartDashboardKey = getDashboardKey(command, processName);
         SmartDashboard.putNumber(smartDashboardKey,SmartDashboard.getNumber(smartDashboardKey,defaultValue)); // put key if it doesn't already exist
+    }
+
+    private static String getDashboardKey(Command command, String processName) {
+        return command==null ? "All_Commands" : command.getName() + "/" + processName + "-wait";
     }
 }
