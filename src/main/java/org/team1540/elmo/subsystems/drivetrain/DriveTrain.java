be621403b4adfb67ab82271e7aaf83be3ce887f4
micahpowch <powchm@catlin.edu>
2022-12-07 18:18:29 -0800

    FX -> SRX, invert motors, put left before right in setPercent args

--- src/main/java/org/team1540/elmo/subsystems/drivetrain/DriveTrain.java
+++ src/main/java/org/team1540/elmo/subsystems/drivetrain/DriveTrain.java
@@ -2,20 +2,25 @@ package org.team1540.elmo.subsystems.drivetrain;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.team1540.elmo.Constants;
 
 public class DriveTrain extends SubsystemBase {
-    private TalonFX frontRight = new TalonFX(Constants.RIGHT_FRONT_MOTOR_CAN_ID);
-    private TalonFX frontLeft = new TalonFX(Constants.LEFT_FRONT_MOTOR_CAN_ID);
-    private TalonFX backRight = new TalonFX(Constants.RIGHT_BACK_MOTOR_CAN_ID);
-    private TalonFX backLeft = new TalonFX(Constants.LEFT_BACK_MOTOR_CAN_ID);
+    private TalonSRX frontRight = new TalonSRX(Constants.RIGHT_FRONT_MOTOR_CAN_ID);
+    private TalonSRX frontLeft = new TalonSRX(Constants.LEFT_FRONT_MOTOR_CAN_ID);
+    private TalonSRX backRight = new TalonSRX(Constants.RIGHT_BACK_MOTOR_CAN_ID);
+    private TalonSRX backLeft = new TalonSRX(Constants.LEFT_BACK_MOTOR_CAN_ID);
 
     public DriveTrain() {
         backLeft.follow(frontLeft);
         backRight.follow(frontRight);
+        frontLeft.setInverted(true);
+        backLeft.setInverted(true);
+        frontRight.setInverted(false);
+        backRight.setInverted(false);
 
         frontRight.setNeutralMode(NeutralMode.Brake);
         frontLeft.setNeutralMode(NeutralMode.Brake);
@@ -25,8 +30,8 @@ public class DriveTrain extends SubsystemBase {
         setPercent(0, 0);
     }
 
-    public void setPercent(double rightPercent, double leftPercent) {
-        frontRight.set(ControlMode.PercentOutput, rightPercent);
+    public void setPercent(double leftPercent, double rightPercent) {
         frontLeft.set(ControlMode.PercentOutput, leftPercent);
+        frontRight.set(ControlMode.PercentOutput, rightPercent);
     }
 }
