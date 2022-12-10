391058d2578574c55add22e51bc4e70e34ebdffe
micahpowch <powchm@catlin.edu>
2022-12-09 23:38:39 -0800

    enable suction at beginning of eject commands

--- src/main/java/org/team1540/elmo/subsystems/ejectors/EjectUpperCommand.java
+++ src/main/java/org/team1540/elmo/subsystems/ejectors/EjectUpperCommand.java
@@ -8,7 +8,8 @@ import org.team1540.elmo.utils.WaitDashboardVarCommand;
 public class EjectUpperCommand extends SequentialCommandGroup {
     public EjectUpperCommand(Ejector.UpperEjector upperEjector) {
         addCommands(
-                new InstantCommand(upperEjector::extend),
+            new InstantCommand(()->{upperEjector.setSuction(true);}),
+            new InstantCommand(upperEjector::extend),
                 new WaitDashboardVarCommand(.3,"extend",this),
                 new InstantCommand(()->upperEjector.setSuction(false))
         );
