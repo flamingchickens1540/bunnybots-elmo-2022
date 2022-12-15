afacb41567af6a65e2fa6e144f2b5c49a2af255d
MeechaPooch <powchm@catlin.edu>
2022-12-14 20:38:15 -0800

    simplify ArcadeDrive to use DriveTrain ramping features

--- src/main/java/org/team1540/elmo/subsystems/drivetrain/ArcadeDrive.java
+++ src/main/java/org/team1540/elmo/subsystems/drivetrain/ArcadeDrive.java
@@ -20,19 +20,8 @@ public class ArcadeDrive extends CommandBase {
     public void execute() {
         double spinPercent = driver.getRightX();
         double forwardPercent = driver.getLeftY();
-        
 
-        double leftPercent = -spinPercent + forwardPercent;
-        double rightPercent = spinPercent + forwardPercent;
-
-        // Any calculated output is greater than 1, scale both coefficients down so that the max is 1
-        double maxScalarPercent = Math.max(Math.abs(leftPercent),Math.abs(rightPercent));
-        if(maxScalarPercent > 1) {
-            double coefficient = 1 / maxScalarPercent;
-            leftPercent *= coefficient;
-            rightPercent *= coefficient;
-        }
-
-        driveTrain.setPercent(leftPercent, rightPercent);
+        // all shit handled in drivetrain functions
+        driveTrain.setSpeedAndSpinRamped(forwardPercent, spinPercent);
     }
 }
